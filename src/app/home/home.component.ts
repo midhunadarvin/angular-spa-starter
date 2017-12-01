a2cd0eaa49a2bf1eaa4b2b891281c58bb4d78ef8
Midhun A Darvin <midhunadarvin@gmail.com>
2017-12-01 07:39:49 +0530

    UI Changes
    - Remove google-map from home page
    - form html change as per bootstrap 4.0.0 - beta 2 specs
    - Remove Agm ( Angular google Map ) module from app.module.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,6 @@
-import * as $ from 'jquery';
 import { Component, OnInit, ViewChild, ElementRef, NgZone } from '@angular/core';
-import { Http } from '@angular/http';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
-import { } from 'googlemaps';
-import { MapsAPILoader } from '@agm/core';
+import { Http } from '@angular/http';
 
 import { DataService } from '../services/data.service';
 import { GlobalService } from '../services/global.service';
@@ -19,10 +16,6 @@ export class HomeComponent implements OnInit {
 
 	cats = [];
 	isLoading = true;
-	title: string = 'My first AGM project';
-	latitude: number = 51.678418;
-	longitude: number = 7.809007;
-	zoom: number;
 	searchControl = new FormControl();
 	cat = {};
 	isEditing = false;
@@ -39,8 +32,6 @@ export class HomeComponent implements OnInit {
 		private http: Http,
 		private dataService: DataService,
 		private formBuilder: FormBuilder,
-		private mapsAPILoader: MapsAPILoader,
-		private ngZone: NgZone,
 		private globalService: GlobalService) { }
 
 	ngOnInit() {
@@ -52,29 +43,6 @@ export class HomeComponent implements OnInit {
 			weight: this.weight
 		});
 
-		//load Places Autocomplete
-		this.mapsAPILoader.load().then(() => {
-			console.log(google.maps);
-			let autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement, {
-				types: ["address"]
-			});
-			autocomplete.addListener("place_changed", () => {
-				this.ngZone.run(() => {
-					//get the place result
-					let place: google.maps.places.PlaceResult = autocomplete.getPlace();
-
-					//verify result
-					if (place.geometry === undefined || place.geometry === null) {
-						return;
-					}
-
-					//set latitude, longitude and zoom
-					this.latitude = place.geometry.location.lat();
-					this.longitude = place.geometry.location.lng();
-					this.zoom = 12;
-				});
-			});
-		});
 	}
 
 	ngAfterViewInit() {
@@ -85,7 +53,7 @@ export class HomeComponent implements OnInit {
 			q: 'angular',
 			sort: 'stars',
 			page: 1,
-			per_page: 10
+			per_page: 6
 		};
 		this.dataService.searchRepos(inputParams).subscribe(
 			data => this.repos = data.items,
